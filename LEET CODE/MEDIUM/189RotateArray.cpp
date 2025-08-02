f3ad9a06a18403b732019b5ae87050a265f4cec5
souma9830 <prabirkumarshee8@gmail.com>
2025-08-02 09:18:52 +0530

    done

---
+++ LEET CODE/MEDIUM/189RotateArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+
+ 
+    void reverse(vector<int>& arr, int i, int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+            
+        }
+        
+    }
+    
+    void rotate(vector<int>& arr, int d) {
+        int n=arr.size();
+        d=d%n;
+        if(d<0) return;
+        
+            reverse(arr, n-d, n-1);
+            reverse(arr, 0, n-d-1);
+            reverse(arr, 0, n-1);
+        
+   
+        
+    }
+};
