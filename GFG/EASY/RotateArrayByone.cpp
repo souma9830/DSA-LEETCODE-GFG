c6c80cb639c5c971dcecf4e821ae8844540cff10
souma9830 <prabirkumarshee8@gmail.com>
2025-08-04 01:16:43 +0530

    done

--- GFG/EASY/RotateArrayByone.cpp
+++ GFG/EASY/RotateArrayByone.cpp
@@ -16,3 +16,16 @@ class Solution {
 
 //for vs code
 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int last=arr[4];
+    for(int i=4;i>0;i--){
+      arr[i]=arr[i-1];
+    }
+   arr[0]=last;
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+}
