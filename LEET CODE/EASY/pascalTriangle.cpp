80d9e9144daa2d664c8eb7b4af5614cf4d83e89b
souma9830 <prabirkumarshee8@gmail.com>
2025-08-04 00:12:27 +0530

    update

--- LEET CODE/EASY/pascalTriangle.cpp
+++ LEET CODE/EASY/pascalTriangle.cpp
@@ -32,3 +32,28 @@ int main(){
 
 
 }
+
+// for leetcode
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector< vector<int> > res;
+        int m=numRows;
+        for(int i=0;i<m;i++){
+            vector<int> a(i+1);
+            res.push_back(a);
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    res[i][j]=1;
+                }
+                else{
+                   res[i][j]=res[i-1][j]+res[i-1][j-1];
+                }
+            }
+        }
+        return res ;
+        
+    }
+};
