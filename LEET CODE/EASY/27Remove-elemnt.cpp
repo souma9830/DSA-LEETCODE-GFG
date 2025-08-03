1ffe197f351a00e8bf391b4f2dcc8d6b72659096
souma9830 <prabirkumarshee8@gmail.com>
2025-08-04 00:09:42 +0530

    updated

--- LEET CODE/EASY/27Remove-elemnt.cpp
+++ LEET CODE/EASY/27Remove-elemnt.cpp
@@ -13,3 +13,42 @@ public:
         
     }
 };
+
+// For run in vs code
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the array size";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter The array Element ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int del;
+    cout<<"Enter the Number which you want to delete";
+    cin>>del;
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==del){
+            count++;
+        }
+    }
+
+    cout<<"The value of count is "<<count;
+    cout<<endl;
+    int ele=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=del){
+            cout<<arr[i]<<" ";
+            ele++;
+        }
+    }
+    cout<<endl;
+    cout<<"The element are "<<ele;
+    
+}
