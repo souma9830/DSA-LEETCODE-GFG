57a3f3b3dcc211c4c6b5afdb3fde20aaa42fe36c
souma9830 <prabirkumarshee8@gmail.com>
2025-08-12 22:52:28 +0530

    chnages

--- LEET CODE/EASY/242ValidAnagram.cpp
+++ LEET CODE/EASY/242ValidAnagram.cpp
@@ -7,3 +7,13 @@ public:
         else return false;
     }
 };
+
+//another approach
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+    }
+};
