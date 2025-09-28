4f0efbfc6159b33d611f77b0c36ee568e202012d
souma9830 <prabirkumarshee8@gmail.com>
2025-09-28 12:44:48 +0530

    done

---
+++ LEET CODE/MEDIUM/61.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        ListNode* temp=head;
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* tail=nullptr;
+        int len=0;
+        while(temp!=NULL){
+            if(temp->next==NULL) tail=temp;
+            temp=temp->next;
+            len++;
+        }
+        k=k%len;
+        if(k==0) return head;
+        temp=head;
+        int travel=len-k;
+        for(int i=1;i<travel;i++){
+            temp=temp->next;
+        }
+        tail->next=head;
+        head=temp->next;
+        temp->next=NULL;
+        return head;
+    }
+};
